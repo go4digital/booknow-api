c58ef853ee39d709908f2917e9c3da695fe6b6b0
ktyadav <kartaryadav@live.in>
2021-08-04 22:27:39 +0530

    Written unit test cases of leads dao, still not working

--- dao/leads_test.go
+++ dao/leads_test.go
@@ -2,14 +2,17 @@ package dao
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"reflect"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go4digital/booknow-api/model"
 	"github.com/stretchr/testify/assert"
 )
 
-var lead = &Lead{
+var lead = &model.Lead{
 	ID:          1,
 	FirstName:   "Test",
 	LastName:    "User",
@@ -27,29 +30,62 @@ func NewMock() (*sql.DB, sqlmock.Sqlmock) {
 	return db, mock
 }
 
-func TestInsertLead(t *testing.T) {
+func TestCreateLead(t *testing.T) {
 	db, mock := NewMock()
+
+	defer db.Close()
+
 	mockleads := NewLeads(db)
-	query := `insert into "leads"("firstname", "lastname", "email", "phone", "description")
-	values($1, $2, $3, $4, $5) RETURNING id`
+
+	query := "insert into leads"
 
 	prep := mock.ExpectPrepare(query)
-	prep.ExpectExec().WithArgs(lead.FirstName, lead.LastName, lead.Email, lead.Phone, lead.Description).WillReturnResult(sqlmock.NewResult(0, 1))
-	id, _ := mockleads.CreateLead(lead)
-	assert.Equal(t, id, 1)
+	prep.ExpectExec().WithArgs(lead.FirstName, lead.LastName, lead.Email, lead.Phone, lead.Description).WillReturnResult(sqlmock.NewResult(1, 1))
+	id, err := mockleads.CreateLead(lead)
+	if err != nil {
+		fmt.Println("this is the error message: ", err)
+		t.Errorf("CreateLead() error = %v, wantErr %v", err, 1)
+		return
+	} else {
+		assert.Equal(t, id, 1)
+	}
 }
 
-func TestGetLead(t *testing.T) {
+func TestGetAllLeads(t *testing.T) {
 	db, mock := NewMock()
 	mockleads := NewLeads(db)
-	query := "SELECT id, firstname, lastname, email, phone, description FROM leads"
 
-	rows := mock.NewRows([]string{"id", "firstname", "lastname", "email", "phone", "description"}).
-		AddRow(lead.ID, lead.FirstName, lead.LastName, lead.Email, lead.Phone, lead.Description)
+	want := []*model.Lead{
+		{
+			ID:          1,
+			FirstName:   "lead1",
+			LastName:    "test1",
+			Email:       "lead1.test1@test.com",
+			Phone:       "25625526",
+			Description: "Need cleaning service",
+		},
+		{
+			ID:          2,
+			FirstName:   "lead2",
+			LastName:    "test2",
+			Email:       "lead2.test2@test.com",
+			Phone:       "1212122",
+			Description: "Need cleaning service",
+		},
+	}
+	// query := "SELECT id, firstname, lastname, email, phone, description FROM leads"
 
-	mock.ExpectQuery(query).WillReturnRows(rows)
+	rows := sqlmock.NewRows([]string{"id", "firstname", "lastname", "email", "phone", "description"}).AddRow(1, "lead1", "test1", "lead1.test1@test.com", "25625526", "Need cleaning service").AddRow(2, "lead2", "test2", "lead2.test2@test.com", "1212122", "Need cleaning service")
 
-	leads, err := mockleads.GetAllLeads()
-	assert.NotNil(t, leads)
-	assert.NoError(t, err)
+	mock.ExpectPrepare("SELECT (.+) FROM leads").ExpectQuery().WillReturnRows(rows)
+
+	got, err := mockleads.GetAllLeads()
+
+	if (err != nil) != true {
+		t.Errorf("GetAllLeads() error new = %v, wantErr %v", err, true)
+		return
+	}
+	if err == nil && !reflect.DeepEqual(got, want) {
+		t.Errorf("GetAllLeads() = %v, want %v", got, want)
+	}
 }
